ae28f1a79e539c571ee6178d473509ec51ea4a91
Albert Darmawan <darmawanalbert@gmail.com>
2021-10-19 01:46:05 +0700

    dev(Base): Footer and Pages Navigation

--- gass-ppi-app/__init__.py
+++ gass-ppi-app/__init__.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/ppi-search")
+def ppi_search():
+    return render_template("ppi-search.html")
+
+@app.route("/help")
+def help():
+    return render_template("help.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/acknowledgements")
+def acknowledgements():
+    return render_template("acknowledgements.html")
+
 if __name__ == "__main__":
     app.run()
